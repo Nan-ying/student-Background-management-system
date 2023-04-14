31d76d35bd9eff491fab915597c2ea4aec959709
nanying <wangxinwan_wy@163.com>
2023-04-14 12:46:10 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "./assets/css/reset.css";
+import "font-awesome/css/font-awesome.min.css";
+import router from "./router";
+import service from "./service/service";
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.prototype.service = service; // 挂载到原型，可以在全局中使用
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://1.116.64.64:5004/api2",
+        changeOrigin: true, // 允许跨域
+        pathRewrite: {
+          "^/api": "",
+        },
+      },
+    },
+  },
+};
